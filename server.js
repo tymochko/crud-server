9245acc2afd7b1f96b17a6b9e79a731feb103a4d
Yuri Tymochko <rain.yura@gmail.com>
2016-05-28 23:06:27 +0300

    Current state: you can add a new user which will be added to JSON database

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var bodyParser = require('body-parser');
 var app = express();
 var config = require('./config');
 var exphbs = require('express-handlebars');
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
 var db = require('./db.js');
 var fs = require('fs');
+var indexTmpl = require('./db/index-template.json');
 
 app.engine('handlebars', exphbs({defaultLayout: 'index'}));
 app.set('view engine', 'handlebars');
@@ -13,6 +16,8 @@ app.set('views', (__dirname + '/views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
+// app.use('/', routes);
+// app.use('/users', users);
 
 // app.use(function(req, res, next) {
 //     var err = new Error('Not Found');
@@ -21,26 +26,24 @@ app.use(express.static(__dirname + '/public'));
 // });
 
 app.get('/', function(req, res) {
-    db.read(function (data) {
-        console.log(data);
-        res.render('form', data)
-    })
+    res.render('form', indexTmpl)
 });
 
 app.post('/', function(req, res) {
     db.read(function (data) {
-        var tableUsers = data.table__row;
-        // tableUsers.push({
-        //     name: req.body.table__row.name,
-        //     surname: req.body.table__row.surname,
-        //     age: req.body.table__row.age
-        // });
+        var tableUsers = data;
+        tableUsers.push({
+            name: req.body.name,
+            surname: req.body.surname,
+            age: req.body.age
+        });
 
-        // console.log(tableUsers);
-
-        // db.write(JSON.stringify(tableUsers), function () {
-        //     res.redirect('/');
-        // });
+        console.log(tableUsers);
+        db.write(JSON.stringify(tableUsers), function () {
+            // res.redirect('/');
+            res.render('form', indexTmpl);
+            res.render('users', tableUsers);
+        });
     });
 });
 
